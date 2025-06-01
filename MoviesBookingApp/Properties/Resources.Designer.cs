40b264a574194547cc8b57e193ca9f1429d83777
SujalGajera <Sujalgajera01@gmail.com>
2025-06-01 12:08:18 +1200

    Sujal : Added background image to prime cinema dashboard

--- MoviesBookingApp/Properties/Resources.Designer.cs
+++ MoviesBookingApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MoviesBookingApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Cinema_Back {
+            get {
+                object obj = ResourceManager.GetObject("Cinema Back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap F1 {
             get {
                 object obj = ResourceManager.GetObject("F1", resourceCulture);
