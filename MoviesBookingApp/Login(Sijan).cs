41aa4b04fb5a7a0f19289b0430ded949400db754
sijan <sijanparajuli5@gmail.com>
2025-06-05 09:52:59 +1200

    Sijan: linked to sign up form

--- MoviesBookingApp/Login(Sijan).cs
+++ MoviesBookingApp/Login(Sijan).cs
@@ -97,6 +97,11 @@ namespace MoviesBookingApp
         {
 
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            new SignUp_Sujal_().Show();
+        }
     }
 }
 
