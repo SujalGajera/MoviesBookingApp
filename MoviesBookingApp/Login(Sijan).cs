42bebe2e449ad6848dbbbfae28e244679f8b30ba
SujalGajera <Sujalgajera01@gmail.com>
2025-06-12 12:03:08 +1200

    Sijan: Connected the sign in with Signup.

--- MoviesBookingApp/Login(Sijan).cs
+++ MoviesBookingApp/Login(Sijan).cs
@@ -101,6 +101,7 @@ namespace MoviesBookingApp
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            new SignUp_Sujal_().Show();
         }
     }
 }
