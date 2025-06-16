af52e37d84eb06212affa573e70407e4edbea065
sijan <sijanparajuli5@gmail.com>
2025-06-16 14:22:10 +1200

    Sijan: Finalised Design for Booking Confirmation.

--- MoviesBookingApp/BookingConfirmation.Designer.cs
+++ MoviesBookingApp/BookingConfirmation.Designer.cs
@@ -28,49 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblDateTime = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
             this.lblSeat = new System.Windows.Forms.Label();
             this.lblMovie = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblSuccess = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // lblTime
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(449, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(314, 39);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Booking confirmation";
-            // 
-            // lblDateTime
-            // 
-            this.lblDateTime.AutoSize = true;
-            this.lblDateTime.Location = new System.Drawing.Point(427, 422);
-            this.lblDateTime.Name = "lblDateTime";
-            this.lblDateTime.Size = new System.Drawing.Size(76, 20);
-            this.lblDateTime.TabIndex = 1;
-            this.lblDateTime.Text = "labelTime";
-            this.lblDateTime.Click += new System.EventHandler(this.label1_Click);
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTime.Location = new System.Drawing.Point(699, 266);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(64, 31);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Time";
+            this.lblTime.Click += new System.EventHandler(this.label1_Click);
             // 
             // lblSeat
             // 
             this.lblSeat.AutoSize = true;
-            this.lblSeat.Location = new System.Drawing.Point(445, 457);
+            this.lblSeat.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold);
+            this.lblSeat.Location = new System.Drawing.Point(421, 374);
             this.lblSeat.Name = "lblSeat";
-            this.lblSeat.Size = new System.Drawing.Size(43, 20);
+            this.lblSeat.Size = new System.Drawing.Size(63, 31);
             this.lblSeat.TabIndex = 2;
             this.lblSeat.Text = "Seat";
             // 
             // lblMovie
             // 
             this.lblMovie.AutoSize = true;
-            this.lblMovie.Location = new System.Drawing.Point(445, 377);
+            this.lblMovie.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold);
+            this.lblMovie.Location = new System.Drawing.Point(410, 277);
             this.lblMovie.Name = "lblMovie";
-            this.lblMovie.Size = new System.Drawing.Size(50, 20);
+            this.lblMovie.Size = new System.Drawing.Size(74, 31);
             this.lblMovie.TabIndex = 3;
             this.lblMovie.Text = "Movie";
             // 
@@ -78,32 +72,43 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::MoviesBookingApp.Properties.Resources.ovir1;
-            this.pictureBox1.Location = new System.Drawing.Point(386, 135);
+            this.pictureBox1.Location = new System.Drawing.Point(177, 86);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(131, 102);
+            this.pictureBox1.Size = new System.Drawing.Size(292, 171);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // lblSuccess
             // 
             this.lblSuccess.AutoSize = true;
-            this.lblSuccess.Location = new System.Drawing.Point(427, 497);
+            this.lblSuccess.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold);
+            this.lblSuccess.Location = new System.Drawing.Point(664, 374);
             this.lblSuccess.Name = "lblSuccess";
-            this.lblSuccess.Size = new System.Drawing.Size(165, 20);
+            this.lblSuccess.Size = new System.Drawing.Size(249, 31);
             this.lblSuccess.TabIndex = 5;
             this.lblSuccess.Text = "Booking Confirmation ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 28F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(551, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(618, 78);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Booking Confirmation ";
             // 
             // BookingConfirmation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1202, 968);
+            this.ClientSize = new System.Drawing.Size(1418, 968);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblSuccess);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblMovie);
             this.Controls.Add(this.lblSeat);
-            this.Controls.Add(this.lblDateTime);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTime);
             this.Name = "BookingConfirmation";
             this.Text = "BookingConfirmation";
             this.Load += new System.EventHandler(this.BookingConfirmation_Load);
@@ -114,12 +119,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label lblDateTime;
+        private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Label lblSeat;
         private System.Windows.Forms.Label lblMovie;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblSuccess;
+        private System.Windows.Forms.Label label1;
     }
 }
