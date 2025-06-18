0787c83477f7bc94342261f6ba60d5bc910a3cb0
SujalGajera <Sujalgajera01@gmail.com>
2025-06-18 19:46:33 +1200

    Sujal: some edits in thank you page

--- MoviesBookingApp/ThankYou(Prabh).Designer.cs
+++ MoviesBookingApp/ThankYou(Prabh).Designer.cs
@@ -52,9 +52,9 @@
             this.labelSuggestion.BackColor = System.Drawing.Color.Transparent;
             this.labelSuggestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSuggestion.ForeColor = System.Drawing.Color.White;
-            this.labelSuggestion.Location = new System.Drawing.Point(211, -116);
+            this.labelSuggestion.Location = new System.Drawing.Point(237, -145);
             this.labelSuggestion.Name = "labelSuggestion";
-            this.labelSuggestion.Size = new System.Drawing.Size(454, 36);
+            this.labelSuggestion.Size = new System.Drawing.Size(550, 40);
             this.labelSuggestion.TabIndex = 10;
             this.labelSuggestion.Text = "You might also like these movies:";
             this.labelSuggestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,9 +65,9 @@
             this.labelThankYou.BackColor = System.Drawing.Color.Transparent;
             this.labelThankYou.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelThankYou.ForeColor = System.Drawing.Color.White;
-            this.labelThankYou.Location = new System.Drawing.Point(281, -206);
+            this.labelThankYou.Location = new System.Drawing.Point(316, -258);
             this.labelThankYou.Name = "labelThankYou";
-            this.labelThankYou.Size = new System.Drawing.Size(388, 69);
+            this.labelThankYou.Size = new System.Drawing.Size(463, 82);
             this.labelThankYou.TabIndex = 9;
             this.labelThankYou.Text = "THANK YOU";
             this.labelThankYou.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -80,7 +80,7 @@
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1260, 64);
+            this.panel4.Size = new System.Drawing.Size(1418, 80);
             this.panel4.TabIndex = 38;
             // 
             // label1
@@ -88,87 +88,105 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(11, 7);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(313, 48);
+            this.label1.Size = new System.Drawing.Size(367, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "PRIME CINEMAS";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(362, 89);
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(407, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(492, 48);
+            this.label2.Size = new System.Drawing.Size(577, 55);
             this.label2.TabIndex = 1;
             this.label2.Text = "Thank you! Come back again!";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(334, 147);
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(376, 184);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(559, 48);
+            this.label3.Size = new System.Drawing.Size(650, 55);
             this.label3.TabIndex = 39;
             this.label3.Text = "Here are some upcoming movies ";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox1.Location = new System.Drawing.Point(85, 246);
+            this.pictureBox1.BackgroundImage = global::MoviesBookingApp.Properties.Resources.M1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(96, 308);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(215, 272);
+            this.pictureBox1.Size = new System.Drawing.Size(242, 340);
             this.pictureBox1.TabIndex = 40;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox2.Location = new System.Drawing.Point(364, 246);
+            this.pictureBox2.BackgroundImage = global::MoviesBookingApp.Properties.Resources.M2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(410, 308);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(215, 272);
+            this.pictureBox2.Size = new System.Drawing.Size(242, 340);
             this.pictureBox2.TabIndex = 41;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox3.Location = new System.Drawing.Point(643, 246);
+            this.pictureBox3.BackgroundImage = global::MoviesBookingApp.Properties.Resources.M3;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(723, 308);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(215, 272);
+            this.pictureBox3.Size = new System.Drawing.Size(242, 340);
             this.pictureBox3.TabIndex = 42;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox4.Location = new System.Drawing.Point(922, 246);
+            this.pictureBox4.BackgroundImage = global::MoviesBookingApp.Properties.Resources.M4;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(1037, 308);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(215, 272);
+            this.pictureBox4.Size = new System.Drawing.Size(242, 340);
             this.pictureBox4.TabIndex = 43;
             this.pictureBox4.TabStop = false;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(452, 621);
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(507, 774);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(326, 58);
+            this.button1.Size = new System.Drawing.Size(458, 72);
             this.button1.TabIndex = 44;
-            this.button1.Text = "Back to main menu";
+            this.button1.Text = "Back to Main Menu";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // ThankYou_Prabh_
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1260, 774);
+            this.BackgroundImage = global::MoviesBookingApp.Properties.Resources.C5C1478FD1762096ACCB619DCEB0F3B4__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1418, 968);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -179,6 +197,7 @@
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.labelSuggestion);
             this.Controls.Add(this.labelThankYou);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ThankYou_Prabh_";
             this.Text = "ThankYou_Prabh_";
             this.panel4.ResumeLayout(false);
